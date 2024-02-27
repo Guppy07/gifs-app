8de4ff4b10931dafb9faa880037b5dba2b28f537
Nicolas <nicolas.jorge.munoz@gmail.com>
2024-02-27 12:55:41 -0300

    clase 90

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
-  template: ` <h5>Buscar:</h5>
-    <input type="text" class="form-control" placeholder="Buscar Gifs..." />`,
+  template: `
+    <h5>Buscar:</h5>
+    <input
+      type="text"
+      class="form-control"
+      placeholder="Buscar Gifs..."
+      (keyup.enter)="searchTag()"
+      #txtTagInput
+    />
+  `,
 })
 export class SearchBoxComponent {
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
   constructor() {}
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log(newTag);
+  }
 }
