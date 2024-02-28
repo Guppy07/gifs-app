e67d33083b9792c8180b16250926c1d722cad6b5
Nicolas <nicolas.jorge.munoz@gmail.com>
2024-02-27 21:50:12 -0300

    clase 95

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
+  public gifList: Gif[] = [];
   private _tagsHistory: string[] = [];
   private apiKey: string = '0o1s4CwrPdoZUJ33qtgnyTj14i42wzub';
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
@@ -31,7 +33,10 @@ export class GifsService {
       .set('limit', '10')
       .set('q', tag);
     this.http
-      .get(`${this.serviceUrl}/search`, { params })
-      .subscribe((resp) => console.log(resp));
+      .get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe((resp) => {
+        this.gifList = resp.data;
+        console.log({ gifs: this.gifList });
+      });
   }
 }
